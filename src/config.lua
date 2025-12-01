6aa260e99ead6a88253c1cab1c14eef4afb53b41
zanncdwbl <xspelz@gmail.com>
2025-12-01 00:29:54 +0000

    2.0.0, move all to lovely?

--- src/config.lua
+++
@@ -1,6 +0,0 @@
----@meta KeepsakeExtender-zannc-config
-return {
-    enabled = true,
-}, {
-    enabled = "Set to true to enable the mod, false to disable."
-}
--- src/ready.lua
+++
@@ -1,6 +0,0 @@
----@meta _
----@diagnostic disable: lowercase-global
-
-modutil.mod.Path.Override("OpenKeepsakeRackScreen", function(base, source)
-	OpenKeepsakeRackScreen_override(base, source)
-end)
